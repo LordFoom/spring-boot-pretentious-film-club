8b08c17ab2800d89c294286508bb013f1dbdea3b
LordFoom <timothys@soliditech.com>
2020-09-05 19:54:28 +0200

    Adds another test findByName

--- src/test/java/lordfoom/sideprojects/pretentiousfilmclub/movie/MovieRepositoryTest.java
+++ src/test/java/lordfoom/sideprojects/pretentiousfilmclub/movie/MovieRepositoryTest.java
@@ -12,9 +12,9 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest
 @DataJpaTest
@@ -26,33 +26,10 @@ class MovieRepositoryTest {
 
     @Autowired
     MovieRepository movieRepository;
+    private static final String SECOND_MOVIE_NAME = "The Semantic Window Breaker";
 
     @BeforeEach
     void setup() {
-//        LocalDate localReleaseDate = LocalDate.of(2020, 8, 24);
-//        Date releaseDate = Date.from(localReleaseDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-//        Movie movie = Movie.builder()
-//                           .description("Test movie Review")
-//                           .name(FOOM_S_FURIOUS_FUNRIDE)
-////                           .id(1L)
-//                           .created(new Date())
-//                           .released(releaseDate).build();
-////        entityManager.persist(movie);
-//        movieRepository.save(movie);
-//        Movie movie_twee = Movie.builder()
-//                           .description("This movie makes the double grade")
-//                           .name("The Semantic Window Breaker")
-////                           .id(2L)
-//                           .created(new Date())
-//                           .released(releaseDate).build();
-//        movieRepository.save(movie);
-////        entityManager.persist(movie_twee);
-//
-////        entityManager.flush();
-    }
-
-    @Test
-    void findAllByNameContaining() {
         LocalDate localReleaseDate = LocalDate.of(2020, 8, 24);
         Date releaseDate = Date.from(localReleaseDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         Movie movie = Movie.builder()
@@ -62,6 +39,34 @@ class MovieRepositoryTest {
                            .created(new Date())
                            .released(releaseDate).build();
         entityManager.persist(movie);
+//        movieRepository.save(movie);
+        Movie movie_twee = Movie.builder()
+                                .description("This movie makes the double grade")
+                                .name(SECOND_MOVIE_NAME)
+//                           .id(2L)
+                                .created(new Date())
+                                .released(releaseDate).build();
+//        movieRepository.save(movie);
+        entityManager.persist(movie_twee);
+//        entityManager.flush();
+    }
+
+    @AfterEach
+    void teardown(){
+        movieRepository.deleteAll();
+    }
+
+    @Test
+    void findAllByNameContaining() {
+//        LocalDate localReleaseDate = LocalDate.of(2020, 8, 24);
+//        Date releaseDate = Date.from(localReleaseDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+//        Movie movie = Movie.builder()
+//                           .description("Test movie Review")
+//                           .name(FOOM_S_FURIOUS_FUNRIDE)
+////                           .id(1L)
+//                           .created(new Date())
+//                           .released(releaseDate).build();
+//        entityManager.persist(movie);
 //        movieRepository.save(movie);
         List<Movie> movieList = movieRepository.findAllByNameContaining("Foom");
         assertEquals(movieList.size(), 1);
@@ -73,5 +78,8 @@ class MovieRepositoryTest {
 
     @Test
     void findByName() {
+        Optional<Movie> byName = movieRepository.findByName(SECOND_MOVIE_NAME);
+        assertTrue(byName.isPresent());
+        assertEquals(SECOND_MOVIE_NAME, byName.get().getName());
     }
 }
